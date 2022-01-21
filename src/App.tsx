62307e250a074acd4c964b427916a7bc192d9887
pawelpyc154@gmail.com <pawelpyc154@gmail.comnpm>
2022-01-21 19:23:21 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import "twin.macro";
-import "styled-components/macro";
+import React from 'react'
+import 'twin.macro'
+import 'styled-components/macro'
 
 function App() {
-  return <div tw="bg-black text-white">test</div>;
+  return <div tw="bg-black text-white">test</div>
 }
 
-export default App;
+export default App
